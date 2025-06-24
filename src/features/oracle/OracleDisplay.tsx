be8285d689b3bd8097f19899d8ff0cd649434b69
droger <david.roger@dashlane.com>
2025-06-24 13:45:54 +0200

    refactor: enhance local storage integration in ActorPage and OracleDisplay components

--- src/features/oracle/OracleDisplay.tsx
+++ src/features/oracle/OracleDisplay.tsx
@@ -3,12 +3,18 @@ import { rollOnTable } from "../../utils/rolls";
 import { Table } from "../../types/type";
 import Button from "../../components/CustomButton";
 import Div from "../../components/CustomDiv";
+import {
+  loadFromLocalStorage,
+  storeItemsInLocalStorage,
+} from "../../utils/localStorageState";
 
 type OracleProps = {
   oracle: Table;
   index?: number;
   modifier?: number;
 };
+
+const ORACLE_SHOW_DETAILS_STORAGE_KEY = "oracleShowDetails";
 
 const displayOracleContent = (oracle: Table) => {
   return (
@@ -31,19 +37,35 @@ const displayOracleContent = (oracle: Table) => {
 
 const OracleDisplay = ({ oracle, index = 1, modifier = 0 }: OracleProps) => {
   const [oracleAnswer, setOracleAnswer] = React.useState<string | null>("-");
+  const [showDetails, setShowDetails] = React.useState<boolean | null>(
+    loadFromLocalStorage(ORACLE_SHOW_DETAILS_STORAGE_KEY) || false
+  );
 
   const handleClick = () => {
     setOracleAnswer(rollOnTable(oracle, index, modifier));
+  };
+
+  const handleShowDetails = () => {
+    setShowDetails(!showDetails);
   };
 
   useEffect(() => {
     setOracleAnswer("-");
   }, [oracle]);
 
+  useEffect(
+    () =>
+      storeItemsInLocalStorage(showDetails, ORACLE_SHOW_DETAILS_STORAGE_KEY),
+    [showDetails]
+  );
+
   return (
     <div className="flex flex-col items-center p-1 space-y-2">
       {oracle.description && <p>{oracle.description}</p>}
-      {displayOracleContent(oracle)}
+      <Button size="sm" onClick={handleShowDetails}>
+        {showDetails ? "Hide" : "Show"}
+      </Button>
+      {showDetails && displayOracleContent(oracle)}
       <Button onClick={handleClick}>Roll</Button>
       {oracleAnswer && (
         <div className="text-m font-bold text-center border rounded p-2 w-full bg-gray-50">
