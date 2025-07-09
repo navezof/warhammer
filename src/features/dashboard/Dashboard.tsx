8fec73fa42f1dc75c3f2e931d6c10224897bd15f
droger <david.roger@dashlane.com>
2025-07-09 16:29:57 +0200

    refactor: move drag n drop outside of dashboard

--- src/features/dashboard/Dashboard.tsx
+++ src/features/dashboard/Dashboard.tsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { WidgetItem } from "./WidgetItem";
 import { Widget } from "../../types/type";
 import { storeItemsInLocalStorage } from "../../utils/localStorageState";
 import { useRPGToolboxContext } from "../../RPGToolboxContext";
 import { AddNewWidget } from "../addWidget/AddNewWidget";
-import { Outlet } from "react-router-dom";
-import { monitorForElements } from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
+import { useWidgetDnD } from "../../components/useWidgetDnD";
 
 const DASHBOARD_WIDGET_STORAGE_KEY = "widgets";
 
@@ -18,62 +17,26 @@ const Dashboard: React.FC = () => {
     [widgets]
   );
 
-  useEffect(() => {
-    return monitorForElements({
-      canMonitor({ source }) {
-        return source.data.instanceId === instanceId;
-      },
-      onDrop({ source, location }) {
-        const destination = location.current.dropTargets[0];
-        if (!destination) {
-          console.log("No destination");
-          return;
-        }
-        const destinationSrc = destination.data.src;
-        const startSrc = source.data.src;
-
-        if (typeof destinationSrc !== "string") {
-          return;
-        }
-        if (typeof startSrc !== "string") {
-          return;
-        }
-
-        const updated = [...widgets];
-        const startIndex = widgets.findIndex((w) => w.id === startSrc);
-        const destIndex = widgets.findIndex((w) => w.id === destinationSrc);
-        if (startIndex !== -1 && destIndex !== -1) {
-          const temp = updated[startIndex];
-          updated[startIndex] = updated[destIndex];
-          updated[destIndex] = temp;
-        }
-
-        setWidgets(updated);
-      },
-    });
-  }, [instanceId, widgets]);
+  useWidgetDnD({ instanceId, widgets, setWidgets });
 
   return (
     <div>
-      <main>
-        <Outlet />
-        <div className="p-2">
-          <div className="grid grid-cols-4 gap-2 auto-rows-[24rem]">
-            {widgets.map((widget: Widget) => (
-              <WidgetItem
-                key={widget.id}
-                className="p-2 bg-gray-800 rounded-lg h-full w-full"
-                id={widget.id}
-                type={widget.type}
-                removeWidget={removeWidget}
-                src={widget.id}
-                instanceId={instanceId}
-              ></WidgetItem>
-            ))}
-          </div>
-          <AddNewWidget />
+      <div className="p-2">
+        <div className="grid grid-cols-4 gap-2 auto-rows-[24rem]">
+          {widgets.map((widget: Widget) => (
+            <WidgetItem
+              key={widget.id}
+              className="p-2 bg-gray-800 rounded-lg h-full w-full"
+              id={widget.id}
+              type={widget.type}
+              removeWidget={removeWidget}
+              src={widget.id}
+              instanceId={instanceId}
+            ></WidgetItem>
+          ))}
         </div>
-      </main>
+        <AddNewWidget />
+      </div>
     </div>
   );
 };
