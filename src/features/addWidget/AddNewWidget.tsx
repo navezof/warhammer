b6e5a06c958c8e54c6ee12974b779ddcc74215bf
David ROGER <david.roger15@gmail.com>
2025-07-13 14:04:17 +0200

    refactor: remove unnecessary addNew, add design and align add new widget

--- src/features/addWidget/AddNewWidget.tsx
+++ src/features/addWidget/AddNewWidget.tsx
@@ -1,9 +1,31 @@
-import { AddNew } from "./AddNew";
+import { useRPGToolboxContext } from "../../RPGToolboxContext";
+import { WidgetType } from "../../types/type";
 
 export const AddNewWidget = () => {
+  const { addNew } = useRPGToolboxContext();
+
   return (
-    <div>
-      <AddNew />
+    <div className="bg-gray-800 rounded-lg flex flex-col items-center justify-center">
+      <select
+        className="p-2 bg-gray-50 text-gray-700 rounded border cursor-pointer hover:border-gray-400"
+        defaultValue=""
+        onChange={(e) => {
+          addNew(e.target.value as WidgetType);
+          e.target.value = "";
+        }}
+      >
+        <option value="" disabled hidden>
+          Add a Widget...
+        </option>
+        <option value="oracle">Oracle</option>
+        <option value="fate">Destin</option>
+        <option value="actor">Acteur</option>
+        <option value="thread">Intrigue</option>
+        <option value="npcInteraction">Interaction PNJ</option>
+        <option value="scene">Scène</option>
+        <option value="name">Noms</option>
+        <option value="npcGenerator">Crée un PNJ</option>
+      </select>
     </div>
   );
 };
